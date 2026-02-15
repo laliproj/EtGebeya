1d9d6d8b2cc58695824bcdc5f827a8f28fe9193f
laliproj <lalitechzone@gmail.com>
2026-02-15 10:10:27 +0300

    built UI component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+import AppRouter from './router/AppRouter';
+import { checkAuth } from './store/authSlice';
+import { fetchNotificationsAPI } from './store/notificationSlice';
+import { fetchWishlist } from './store/wishlistSlice';
+
+function App() {
+  const dispatch = useDispatch();
+
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
--- src/components/product/SimilarProducts.jsx
+++ src/components/product/SimilarProducts.jsx
@@ -31,3 +31,14 @@ const SimilarProducts = ({ productId }) => {
         Similar Products
       </h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        {loading
+          ? [...Array(4)].map((_, i) => <ProductCardSkeleton key={i} />)
+          : products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+      </div>
+    </div>
+  );
+};
+
+export default SimilarProducts;
