a0989e5ef0944fcff0ffb7ab7d406de144f51b5f
laliproj <lalitechzone@gmail.com>
2026-03-08 19:23:46 +0300

    added project files

--- src/store/wishlistSlice.js
+++ src/store/wishlistSlice.js
@@ -15,3 +15,20 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import wishlistService from '../services/wishlistService';
 
+export const fetchWishlist = createAsyncThunk(
+  'wishlist/fetch',
+  async (_, { rejectWithValue }) => {
+    try {
+      return await wishlistService.getWishlist(); // returns array of IDs
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const toggleWishlistAPI = createAsyncThunk(
+  'wishlist/toggle',
+  async (productId, { rejectWithValue }) => {
+    try {
+      const response = await wishlistService.toggleWishlist(productId);
+      return { productId, action: response.action };
