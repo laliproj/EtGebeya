b2f5f2297457a9214c6c205525426ec163135ea4
laliproj <lalitechzone@gmail.com>
2026-04-01 16:55:04 +0300

    added 6a120ce4e7d18_1779567844.png

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,9 @@
+import HeroBanner from '../../components/home/HeroBanner';
+import CategoryNav from '../../components/home/CategoryNav';
+import FeaturedProducts from '../../components/home/FeaturedProducts';
+import RecentProducts from '../../components/home/RecentProducts';
+import TrendingBrands from '../../components/home/TrendingBrands';
+import AIRecommendedProducts from '../../components/home/AIRecommendedProducts';
+import AITrendingProducts from '../../components/home/AITrendingProducts';
+
+const HomePage = () => {
