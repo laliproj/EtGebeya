80e4dce7c2dc71aee7a2c33daef21bb5d6580420
laliproj <lalitechzone@gmail.com>
2026-05-05 22:16:18 +0300

    updated configurations

---
+++ backend/helpers/response.php
@@ -0,0 +1,8 @@
+<?php
+// backend/helpers/response.php
+
+// Handle CORS preflight for all endpoints
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
