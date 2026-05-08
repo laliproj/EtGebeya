c2b71313f1dc39e7942d53ca3c0593ba856b9e57
laliproj <lalitechzone@gmail.com>
2026-05-08 13:15:07 +0300

    built UI component

--- backend/helpers/response.php
+++ backend/helpers/response.php
@@ -22,3 +22,11 @@ function jsonResponse($success, $message, $data = [], $statusCode = 200) {
     http_response_code($statusCode);
     header('Content-Type: application/json');
     
+    echo json_encode([
+        'success' => $success,
+        'message' => $message,
+        'data' => $data
+    ]);
+    exit;
+}
+?>
