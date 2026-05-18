32c0c18c727ae225365007ab854a691480f5668f
laliproj <lalitechzone@gmail.com>
2026-05-18 21:13:26 +0300

    added project files

--- src/services/messagesService.js
+++ src/services/messagesService.js
@@ -6,3 +6,11 @@ const messagesService = {
     if (response.data.success) {
       return response.data.data;
     }
+    throw new Error(response.data.message);
+  },
+
+  getHistory: async (userId) => {
+    const response = await api.get(`/messages/history.php?user_id=${userId}`);
+    if (response.data.success) {
+      return response.data.data;
+    }
