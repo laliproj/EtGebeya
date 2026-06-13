fba5fb3a8c546aa2c0749ba8f2d22bbb5859f162
laliproj <lalitechzone@gmail.com>
2026-06-13 15:34:26 +0300

    added animations to skeleton

---
+++ src/components/common/Skeleton.jsx
@@ -0,0 +1,7 @@
+/**
+ * Reusable Skeleton loader component
+ */
+const Skeleton = ({ className = '', variant = 'rectangular' }) => {
+  const variants = {
+    rectangular: 'rounded-xl',
+    circular: 'rounded-full',
