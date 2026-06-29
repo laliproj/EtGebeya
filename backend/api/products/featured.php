5265dc0031cca24c6614de0f0327ff15d11784ca
laliproj <lalitechzone@gmail.com>
2026-06-29 23:20:38 +0300

    continued work on notificationdropdown

---
+++ backend/api/products/featured.php
@@ -0,0 +1,13 @@
+<?php
+// backend/api/products/featured.php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../../helpers/response.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
